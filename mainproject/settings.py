a4d71592b47cd04838dd274f4aa10076250c07e4
rohitbhandari007 <rohitbhandari20561210@gmail.com>
2022-06-19 16:07:22 +0545

    init heroku in django settings

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from threading import local
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +129,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+django_heroku.settings(locals())
